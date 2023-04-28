345493df321018a33351ced39d6d2940b588a377
anoop <anoopbabyathiyadam@gmail.com>
2023-04-29 00:23:37 +0530

    images added in folder

--- src/Datas/data.js
+++ src/Datas/data.js
@@ -1,26 +1,46 @@
+import imag1 from './img/img1.jpg';
+import imag2 from './img/img2.jpg';
+import imag3 from './img/img3.jpg';
+import imag4 from './img/img4.JPG';
+import imag5 from './img/img5.JPG';
+
+
+import imag6 from './img/img6.jpg';
+import imag7 from './img/img7.JPG';
+import imag8 from './img/img8.JPG';
+import imag9 from './img/img9.JPG';
+import imag10 from './img/img10.JPG';
+
+import doc1 from './img/doc1.jpg';
+import doc2 from './img/doc2.jpg';
+import doc3 from './img/doc3.jpg';
+import doc4 from './img/doc4.jpg';
+import doc5 from './img/doc5.jpg';
+
+
+
+
+
 export const pageData = [
     {
         sugTitle: "You might want...",
         sugSubtitle: "Design you",
         suggested: [
             {
-                id: 1, title: "Doc", url: "https://cms-assets.tutsplus.com/cdn-cgi/image/width=630/uploads/users/1631/posts/33068/image/Screen-Shot-2019-04-09-at-00.31.23.jpg",
+                id: 1, title: "Doc", url: imag1,
+            },
+            {
+                id: 2, title:"Your Story", url: imag2,
+            },
+            { 
+                id: 3, title: "Video", url: imag3,
+            },
+            {
+                id: 4, title: "Photo Collage", url: imag4,
 
             },
             {
-                id: 2, title: "Your Story", url: "https://img.freepik.com/premium-vector/clean-new-collection-fashion-sale-landing-pages_7605-445.jpg"
-
-            },
-            {
-                id: 3, title: "Video", url: "https://img.freepik.com/free-vector/business-woman-banner-template_23-2148798237.jpg?w=900&t=st=1682059203~exp=1682059803~hmac=10db3f4f6e2b0dfb9cc60e0232ddf51239bbc2a34829aa5d5765d911d86c54db"
-
-            },
-            {
-                id: 4, title: "Photo Collage", url: "https://img.freepik.com/free-photo/digital-painting-mountain-with-colorful-tree-foreground_1340-25699.jpg?size=626&ext=jpg&ga=GA1.1.1577341204.1682002845"
-
-            },
-            {
-                id: 5, title: "Insta Reel", url: "https://img.freepik.com/free-vector/flat-spring-horizontal-banner-template_23-2149280328.jpg?t=st=1682055655~exp=1682056255~hmac=2b9079ba9d2311d016e6e277a0f7ef9adbb3a011801740fb0e4a6b81455305d5"
+                id: 5, title: "Insta Reel", url: imag5,
 
             }
         ],
@@ -29,22 +49,22 @@ export const pageData = [
         // tmpSubtitle: "555-555-5555",
         template: [
             {
-                id: 1, title: "RecentTemplate1", url: "https://img.freepik.com/free-photo/young-handsome-physician-medical-robe-with-stethoscope_1303-17818.jpg?size=626&ext=jpg&ga=GA1.2.1287520646.1682078036",
+                id: 1, title: "RecentTemplate1", url: imag6
 
             },
             {
-                id: 2, title: "RecentTemplate2", url: "https://img.freepik.com/premium-photo/blue-dramatic-sunset-sky-texture-background_29332-1155.jpg?size=626&ext=jpg&ga=GA1.2.1287520646.1682078036"
+                id: 2, title: "RecentTemplate2", url: imag7
 
             },
             {
-                id: 3, title: "RecentTemplate3", url: "https://img.freepik.com/premium-photo/young-asian-indian-college-students-reading-books-studying-laptop-preparing-exam-working-group-project-while-sitting-grass-staircase-steps-college-campus_466689-7729.jpg?size=626&ext=jpg&ga=GA1.2.1287520646.1682078036"
+                id: 3, title: "RecentTemplate3", url: imag8
             },
             {
-                id: 4, title: "RecentTemplate4", url: "https://img.freepik.com/free-photo/html-css-collage-concept-with-person_23-2150062008.jpg?size=626&ext=jpg&ga=GA1.1.1287520646.1682078036"
+                id: 4, title: "RecentTemplate4", url: imag9
 
             },
             {
-                id: 5, title: "RecentTemplate5", url: "https://img.freepik.com/free-photo/plant-against-blue-wall-background-with-copy-space_53876-98324.jpg?size=626&ext=jpg&ga=GA1.1.1287520646.1682078036"
+                id: 5, title: "RecentTemplate5", url: imag10
 
             }
         ]
@@ -57,23 +77,23 @@ export const pageData = [
         sugSubtitle: "Write Explore",
         suggested: [
             {
-                id: 1, title: "Doc", url: "https://content.wepik.com/statics/13483659/preview-page0.jpg",
+                id: 1, title: "Doc", url: doc1,
 
             },
             {
-                id: 2, title: "Doc to Dec", url: "https://content.wepik.com/statics/39498342/preview-page0.jpg"
+                id: 2, title: "Doc to Dec", url: doc2
 
             },
             {
-                id: 3, title: "PDF", url: "https://content.wepik.com/statics/18682576/preview-page0.jpg"
+                id: 3, title: "PDF", url: doc3
 
             },
             {
-                id: 4, title: "Doc to PDF", url: "https://content.wepik.com/statics/4449529/preview-page0.jpg"
+                id: 4, title: "Doc to PDF", url: doc4
 
             },
             {
-                id: 5, title: "Image to PDF", url: "https://content.wepik.com/statics/10538755/preview-page0.jpg"
+                id: 5, title: "Image to PDF", url: doc5
 
             }
         ],
@@ -82,22 +102,22 @@ export const pageData = [
         tmpSubtitle: "Documents",
         template: [
             {
-                id: 1, title: "DocTemplate1", url: "https://img.freepik.com/free-photo/young-handsome-physician-medical-robe-with-stethoscope_1303-17818.jpg?size=626&ext=jpg&ga=GA1.2.1287520646.1682078036",
+                id: 1, title: "DocTemplate1", url: imag10
 
             },
             {
-                id: 2, title: "DocTemplate2", url: "https://img.freepik.com/premium-photo/blue-dramatic-sunset-sky-texture-background_29332-1155.jpg?size=626&ext=jpg&ga=GA1.2.1287520646.1682078036"
+                id: 2, title: "DocTemplate2", url: imag1
 
             },
             {
-                id: 3, title: "DocTemplate3", url: "https://img.freepik.com/premium-photo/young-asian-indian-college-students-reading-books-studying-laptop-preparing-exam-working-group-project-while-sitting-grass-staircase-steps-college-campus_466689-7729.jpg?size=626&ext=jpg&ga=GA1.2.1287520646.1682078036"
+                id: 3, title: "DocTemplate3", url: imag5
             },
             {
-                id: 4, title: 'DocTemplate4', url: "https://img.freepik.com/free-photo/digital-painting-mountain-with-colorful-tree-foreground_1340-25699.jpg?size=626&ext=jpg&ga=GA1.1.1577341204.1682002845"
+                id: 4, title: 'DocTemplate4', url: imag2
 
             },
             {
-                id: 5, title: 'DocTemplate5', url: "https://img.freepik.com/free-vector/flat-spring-horizontal-banner-template_23-2149280328.jpg?t=st=1682055655~exp=1682056255~hmac=2b9079ba9d2311d016e6e277a0f7ef9adbb3a011801740fb0e4a6b81455305d5"
+                id: 5, title: 'DocTemplate5', url: imag4
 
             }
         ]
@@ -109,22 +129,22 @@ export const pageData = [
         sugSubtitle: "Click Your Moment",
         suggested: [
             {
-                id: 1, title: "Clasi", url: "https://img.freepik.com/free-photo/young-handsome-physician-medical-robe-with-stethoscope_1303-17818.jpg?size=626&ext=jpg&ga=GA1.2.1287520646.1682078036",
+                id: 1, title: "Clasi", url: imag6
 
             },
             {
-                id: 2, title: "Trending", url: "https://img.freepik.com/premium-photo/blue-dramatic-sunset-sky-texture-background_29332-1155.jpg?size=626&ext=jpg&ga=GA1.2.1287520646.1682078036"
+                id: 2, title: "Trending", url: imag7
 
             },
             {
-                id: 3, title: "Wintage", url: "https://img.freepik.com/premium-photo/young-asian-indian-college-students-reading-books-studying-laptop-preparing-exam-working-group-project-while-sitting-grass-staircase-steps-college-campus_466689-7729.jpg?size=626&ext=jpg&ga=GA1.2.1287520646.1682078036"
+                id: 3, title: "Wintage", url: imag8
             },
             {
-                id: 4, title: "Old is Gold", url: "https://img.freepik.com/free-photo/html-css-collage-concept-with-person_23-2150062008.jpg?size=626&ext=jpg&ga=GA1.1.1287520646.1682078036"
+                id: 4, title: "Old is Gold", url: imag9
 
             },
             {
-                id: 5, title: "Free feel snap", url: "https://img.freepik.com/free-photo/plant-against-blue-wall-background-with-copy-space_53876-98324.jpg?size=626&ext=jpg&ga=GA1.1.1287520646.1682078036"
+                id: 5, title: "Free feel snap", url: imag10
 
             }
         ],
@@ -133,23 +153,22 @@ export const pageData = [
         tmpSubtitle: "Photos",
         template: [
             {
-                id: 1, title: 'PhotoCollage', url: "https://cms-assets.tutsplus.com/cdn-cgi/image/width=630/uploads/users/1631/posts/33068/image/Screen-Shot-2019-04-09-at-00.31.23.jpg",
+                id: 1, title: 'PhotoCollage', url: imag3
 
             },
             {
-                id: 2, title: 'Colorfull Pics', url: "https://img.freepik.com/premium-vector/clean-new-collection-fashion-sale-landing-pages_7605-445.jpg"
+                id: 2, title: 'Colorfull Pics', url: imag6
 
             },
             {
-                id: 3, title: 'Greeneee', url: "https://img.freepik.com/free-vector/business-woman-banner-template_23-2148798237.jpg?w=900&t=st=1682059203~exp=1682059803~hmac=10db3f4f6e2b0dfb9cc60e0232ddf51239bbc2a34829aa5d5765d911d86c54db"
+                id: 3, title: 'Greeneee', url: imag1
 
             },
             {
-                id: 4, title: 'Collage3', url: "https://img.freepik.com/free-photo/digital-painting-mountain-with-colorful-tree-foreground_1340-25699.jpg?size=626&ext=jpg&ga=GA1.1.1577341204.1682002845"
-
+                id: 4, title: 'Collage3', url: imag5
             },
             {
-                id: 5, title: 'Black and White', url: "https://img.freepik.com/free-vector/flat-spring-horizontal-banner-template_23-2149280328.jpg?t=st=1682055655~exp=1682056255~hmac=2b9079ba9d2311d016e6e277a0f7ef9adbb3a011801740fb0e4a6b81455305d5"
+                id: 5, title: 'Black and White',url: imag2
 
             }
         ]
@@ -162,22 +181,21 @@ export const pageData = [
         sugSubtitle: "Play and Pause",
         suggested: [
             {
-                id: 1, title: "MP4", url: "https://cms-assets.tutsplus.com/cdn-cgi/image/width=630/uploads/users/1631/posts/33068/image/Screen-Shot-2019-04-09-at-00.31.23.jpg",
+                id: 1, title: "MP4", url: imag3
 
             },
             {
-                id: 2, title: "VLC", url:"https://img.freepik.com/free-vector/business-woman-banner-template_23-2148798237.jpg?w=900&t=st=1682059203~exp=1682059803~hmac=10db3f4f6e2b0dfb9cc60e0232ddf51239bbc2a34829aa5d5765d911d86c54db"
+                id: 2, title: "VLC", url:imag9
 
             },
             {
-                id: 3, title: "Own Player", url: "https://img.freepik.com/premium-vector/clean-new-collection-fashion-sale-landing-pages_7605-445.jpg"
+                id: 3, title: "Own Player", url:imag5
             },
             {
-                id: 4, title: "Start Recording", url: "https://img.freepik.com/free-photo/digital-painting-mountain-with-colorful-tree-foreground_1340-25699.jpg?size=626&ext=jpg&ga=GA1.1.1577341204.1682002845"
-
+                id: 4, title: "Start Recording", url: imag7
             },
             {
-                id: 5, title: "Free feel Smile", url: "https://img.freepik.com/free-vector/flat-spring-horizontal-banner-template_23-2149280328.jpg?t=st=1682055655~exp=1682056255~hmac=2b9079ba9d2311d016e6e277a0f7ef9adbb3a011801740fb0e4a6b81455305d5"
+                id: 5, title: "Free feel Smile", url: imag10
 
             }
         ],
@@ -186,23 +204,23 @@ export const pageData = [
         tmpSubtitle: "Videos",
         template: [
             {
-                id: 1, title: "Youtube Video", url: "https://cms-assets.tutsplus.com/cdn-cgi/image/width=630/uploads/users/1631/posts/33068/image/Screen-Shot-2019-04-09-at-00.31.23.jpg",
+                id: 1, title: "Youtube Video", url: imag1,
 
             },
             {
-                id: 2, title: "Insta Reels", url: "https://img.freepik.com/premium-vector/clean-new-collection-fashion-sale-landing-pages_7605-445.jpg"
+                id: 2, title: "Insta Reels", url: imag2
 
             },
             {
-                id: 3, title: "Facebook reels", url: "https://img.freepik.com/free-vector/business-woman-banner-template_23-2148798237.jpg?w=900&t=st=1682059203~exp=1682059803~hmac=10db3f4f6e2b0dfb9cc60e0232ddf51239bbc2a34829aa5d5765d911d86c54db"
+                id: 3, title: "Facebook reels", url: imag3,
 
             },
             {
-                id: 4, title: "Instand record", url: "https://img.freepik.com/free-photo/digital-painting-mountain-with-colorful-tree-foreground_1340-25699.jpg?size=626&ext=jpg&ga=GA1.1.1577341204.1682002845"
+                id: 4, title: "Instand record", url: imag4,
 
             },
             {
-                id: 5, title: "Insta Story", url: "https://img.freepik.com/free-vector/flat-spring-horizontal-banner-template_23-2149280328.jpg?t=st=1682055655~exp=1682056255~hmac=2b9079ba9d2311d016e6e277a0f7ef9adbb3a011801740fb0e4a6b81455305d5"
+                id: 5, title: "Insta Story", url: imag5,
 
             }
         ]
